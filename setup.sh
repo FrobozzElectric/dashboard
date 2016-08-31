807dc204d735df2f8deaee4fff2b9e2e0a4a0813
FrobozzElectric <gpfau7@gmail.com>
2016-08-31 09:48:24 -0400

    whoops!

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 set -e
 
 packages='sudo vim htop rxvt-unicode x11vnc chromium chromium-l10n xorg openbox unclutter xdotool figlet'
-ip=$(ip addr | grep 'inet ' | grep -v '127.0.0.1' | cut -d' ' -f6)
-hostname=$(hostname)
 
 if [ -z "$(cat /etc/issue | grep "Debian GNU/Linux 8")" ]; then 
     echo "This script was only written to run on Debian Jessie. Exiting..."
@@ -19,6 +17,8 @@ su -c "apt-get update && apt-get install $packages -y && echo \"$USER    ALL=(AL
 echo '#!/bin/bash
 
 links=( $(cat links-to-load.txt | grep -v ^#) )
+ip=$(ip addr | grep 'inet ' | grep -v '127.0.0.1' | cut -d' ' -f6)
+hostname=$(hostname)
 
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/g' $HOME/.config/chromium/Default/Preferences
 xset s off &
