6ffb5c2af8f9aa5f5955c0f209f10a5286885e67
FrobozzElectric <gpfau7@gmail.com>
2016-08-31 10:32:19 -0400

    cleanup

--- setup.sh
+++ setup.sh
@@ -10,10 +10,6 @@ if [ -z "$(cat /etc/issue | grep "Debian GNU/Linux 8")" ]; then
 fi
 
 su -c "apt-get update && apt-get install $packages -y && echo \"$USER    ALL=(ALL:ALL) ALL\" >> /etc/sudoers"
-#su - $USER
-#orig_group=$(id -g)
-#newgrp sudo
-#newgroup $orig_group
 echo '#!/bin/bash
 
 links=( $(cat links-to-load.txt | grep -v ^#) )
@@ -26,7 +22,6 @@ xset -dpms &
 x11vnc -nap -wait 30 -noxdamage -display :0 -forever &
 unclutter -idle 1 -jitter 2 -root &
 urxvt  -e bash -c "echo -e \"hostname: $hostname\\nip: $ip\" | figlet; bash" &
-#chromium-browser \
 chromium \
   --kiosk-mode \
   --auto-launch-at-startup \
@@ -37,7 +32,6 @@ chromium \
   --default-tile-width=384 \
   --default-tile-height=384 \
   $( echo ${links[@]} ) &
-#xdotool search -sync -onlyvisible -class "chromium-browser" key F11
 sleep 15 && xdotool search --onlyvisible --class "Chromium" windowfocus key 'F11' &
 openbox-session' > ~/.xsession
 echo 'if [ -z "$SSH_CLIENT" ] || [ -z "$SSH_TTY" ]; then
