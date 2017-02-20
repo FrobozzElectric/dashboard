db0b3c7f17a348cda4bf90cfa218898ccf6d26bc
FrobozzElectric <gpfau7@gmail.com>
2017-02-20 11:49:01 -0500

    easier restarting

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-packages='sudo vim htop rxvt-unicode x11vnc chromium chromium-l10n xorg openbox unclutter xdotool figlet'
+packages='sudo vim htop rxvt-unicode x11vnc chromium chromium-l10n xorg openbox unclutter xdotool figlet screen'
 
 if [ -z "$(cat /etc/issue | grep "Debian GNU/Linux 8")" ]; then 
     echo "This script was only written to run on Debian Jessie. Exiting..."
@@ -12,7 +12,7 @@ fi
 su -c "apt-get update && apt-get install $packages -y && echo \"$USER    ALL=(ALL:ALL) ALL\" >> /etc/sudoers"
 echo '#!/bin/bash
 
-links=( $(cat links-to-load.txt | grep -v ^#) )
+#links=( $(cat links-to-load.txt | grep -v ^#) )
 
 get_ip() {
     ip addr | grep "inet " | grep -v "127.0.0.1" | cut -d" " -f6
@@ -28,23 +28,39 @@ while [ -z $ip ]; do
 done
 hostname=$(hostname)
 urxvt  -e bash -c "echo -e \"hostname: $hostname\\nip: $ip\" | figlet; bash" &
-chromium \
-  --kiosk-mode \
-  --auto-launch-at-startup \
-  --disable-hang-monitor \
-  --disable-session-crashed-bubble \
-  --disable-infobars \
-  --disable-cache \
-  --disable-canvas-aa \
-  --default-tile-width=384 \
-  --default-tile-height=384 \
-  --disk-cache-dir="/dev/shm/" \
-  $( echo ${links[@]} ) &
+#chromium \
+#  --kiosk-mode \
+#  --auto-launch-at-startup \
+#  --disable-hang-monitor \
+#  --disable-session-crashed-bubble \
+#  --disable-infobars \
+#  --disable-cache \
+#  --disable-canvas-aa \
+#  --default-tile-width=384 \
+#  --default-tile-height=384 \
+#  --disk-cache-dir="/dev/shm/" \
+#  $( echo ${links[@]} ) &
+chromium-start
 sleep 15 && xdotool search --onlyvisible --class "Chromium" windowfocus key F11 &
 openbox-session' > ~/.xsession
 echo 'if [ -z "$SSH_CLIENT" ] || [ -z "$SSH_TTY" ]; then
     startx
-fi' >> ~/.bashrc
+fi
+
+chromium-start () {
+    export DISPLAY=:0
+    links=( $(cat ~/links-to-load.txt | grep -v ^#) )
+    screen chromium \
+      --kiosk-mode \
+      --auto-launch-at-startup \
+      --disable-hang-monitor \
+      --disable-session-crashed-bubble \
+      --disable-cache \
+      --disable-canvas-aa \
+      --default-tile-width=384 \
+      --default-tile-height=384 \
+      $( echo ${links[@]} )
+}' >> ~/.bashrc
 echo 'https://www.google.com/' > ~/links-to-load.txt
 sudo sed -i "s/ExecStart.*/ExecStart=-\/sbin\/agetty -a $USER %I $TERM/g" /etc/systemd/system/getty.target.wants/getty@tty1.service
 sudo systemctl set-default multi-user.target
