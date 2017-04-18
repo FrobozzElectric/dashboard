b09c26e3b9c29435a690f91f1ea1d32d9b517dd2
FrobozzElectric <gpfau7@gmail.com>
2017-04-18 10:47:37 -0400

    documentation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# Check distro version and install packages
 packages='sudo vim htop rxvt-unicode x11vnc chromium chromium-l10n xorg openbox unclutter xdotool figlet screen wmctrl'
 
 if [ -z "$(grep "Debian GNU/Linux 8" /etc/issue)" ]; then 
@@ -10,6 +11,8 @@ if [ -z "$(grep "Debian GNU/Linux 8" /etc/issue)" ]; then
 fi
 
 su -c "apt-get update && apt-get install $packages -y && echo \"$USER    ALL=(ALL:ALL) ALL\" >> /etc/sudoers"
+
+# This creates the .xsession file
 echo '#!/bin/bash
 
 get_ip() {
@@ -29,6 +32,8 @@ urxvt  -e bash -c "echo -e \"hostname: $hostname\\nip: $ip\" | figlet; bash" &
 chromium-start
 sleep 15 && xdotool search --onlyvisible --class "Chromium" windowfocus key F11 &
 openbox-session' > ~/.xsession
+
+# This creates the .bashrc file
 echo 'if [ -z "$SSH_CLIENT" ] || [ -z "$SSH_TTY" ]; then
     startx
 fi
@@ -52,7 +57,11 @@ chromium-stop() {
     export DISPLAY=:0
     wmctrl -c chromium
 }' >> ~/.bashrc
+
+# This creates a default 'links-to-load.txt'. Add your own links!
 echo 'https://www.google.com/' > ~/links-to-load.txt
+
+# Set auto-login for current user via systemd
 sudo sed -i "s/ExecStart.*/ExecStart=-\/sbin\/agetty -a $USER %I $TERM/g" /etc/systemd/system/getty.target.wants/getty@tty1.service
 sudo systemctl set-default multi-user.target
 sudo reboot
