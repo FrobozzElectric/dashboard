b44f7dcca9e3a3e8ac9ad4160e9b5154743deeb9
FrobozzElectric <gpfau7@gmail.com>
2017-04-18 12:04:28 -0400

    standalone scripts

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ get_ip() {
     ip addr | grep "inet " | grep -v "127.0.0.1" | cut -d" " -f6
 }
 
+echo $DISPLAY > ~/DISPLAY
 xset s off &
 xset -dpms &
 x11vnc -nap -wait 30 -noxdamage -display $DISPLAY -forever &
@@ -36,30 +37,35 @@ openbox-session' > ~/.xsession
 # This adds to the .bashrc file
 echo 'if [ -z "$SSH_CLIENT" ] || [ -z "$SSH_TTY" ]; then
     startx
-fi
+fi' >> ~/.bashrc
 
-chromium-start() {
-    export DISPLAY=:0
-    links=( $(cat ~/links-to-load.txt | grep -v ^#) )
-    screen -dm chromium \
-      --kiosk-mode \
-      --auto-launch-at-startup \
-      --disable-hang-monitor \
-      --disable-session-crashed-bubble \
-      --disable-cache \
-      --disable-canvas-aa \
-      --default-tile-width=384 \
-      --default-tile-height=384 \
-      $( echo ${links[@]} )
-}
+# Make chromium-start and chromium-stop executables
+echo '#!/bin/bash
 
-chromium-stop() {
-    export DISPLAY=:0
-    wmctrl -c chromium
-}' >> ~/.bashrc
+export DISPLAY=$(<~/DISPLAY)
+links=( $(cat ~/links-to-load.txt | grep -v ^#) )
+screen -dm chromium \
+    --kiosk-mode \
+    --auto-launch-at-startup \
+    --disable-hang-monitor \
+    --disable-session-crashed-bubble \
+    --disable-cache \
+    --disable-canvas-aa \
+    --default-tile-width=384 \
+    --default-tile-height=384 \
+    $( echo ${links[@]} )' > ~/chromium-start
+
+echo '#!/bin/bash
+
+export DISPLAY=$(<~/DISPLAY)
+wmctrl -c chromium' > ~/chromium-stop
 
 # This creates a default 'links-to-load.txt'. Add your own links!
 echo 'https://www.google.com/' > ~/links-to-load.txt
+
+# Install chromium-start and chromium-stop
+chmod +x ~/chromium-start ~/chromium-stop
+sudo mv ~/chromium-start ~/chromium-stop -t /usr/local/bin/
 
 # Set auto-login for current user via systemd
 sudo sed -i "s/ExecStart.*/ExecStart=-\/sbin\/agetty -a $USER %I $TERM/g" /etc/systemd/system/getty.target.wants/getty@tty1.service
